1658ec5e4ccb9adb7e3414e6050f379603fd8935
Cleiton Oliveira <cleiton.ti@outlook.com>
2021-05-28 20:35:29 -0300

    operadores

--- src/br/com/learnjava/basic/chapC/operadores/OperadorIncremento.java
+++ src/br/com/learnjava/basic/chapC/operadores/OperadorIncremento.java
@@ -1,5 +1,57 @@
 package br.com.learnjava.basic.chapC.operadores;
 
+
+
+/**
+ * 
+ * @author Cleiton Oliveira
+ * 
+ * O operador de incremento é utilizado para incrementar o valor de uma variavel numérica 
+ * antes ou depois de uma operação.
+ * 
+ * O incremento sempre adiciona +1 a variavel.
+ *
+ */
 public class OperadorIncremento {
+	
+	
+	public void incremento() {
+		int varaivelNumerica = 0;
+		
+		// antes de prosseguir entenda o que é uma operação
+		// uma operação e uma execução de código
+		// em uma espressão podem haver varias operações
+		
+		
+		
+		// no exemplo abaixo a variavel é incrementada após a operação
+		// nesse caso temos duas operações 
+		
+		// Operação 1: cria a varaive 'int varaivel2'
+		// Operação 2: atribuimos o valor da variavel 'varaivelNumerica' a variavel 'varaivel2'
+		
+		// sendo assim nosso incremento vai acontecer após a operação, ou seja,
+		// o valor da variavel 'varaivelNumerica' será atribuido a 'varaivel2' e só depois disso a 'varaivelNumerica' reberá +1 ao seu valor
+		int varaivel2 = varaivelNumerica++;
+		
+		
+		
+		// agora nossa variavel 'varaivelNumerica' tem valor 1
+		// e nossa variavel 'varaivel2' tem valor 0
+		
+		// nas duas operações abaixo realizamos incrementos das nossas duas variaveis
+		// assim teremos:
+		// varaivel2 = 1
+		// varaivelNumerica = 2
+		varaivel2++;
+		varaivelNumerica++;
+		
+		
+		// Na operação abaixo realizamos o incremento antes da operação
+		// ou seja, o incremento acontece antes de atribuir o valor de 'varaivelNumerica' a  'varaivel2'
+		// ao fim da execução da expressão as duas variaveis terão o mesmo valor
+		varaivel2 = ++varaivelNumerica;		
+		
+	}
 
 }
--- src/br/com/learnjava/basic/chapC/operadores/OperadoresAritmeticos.java
+++ src/br/com/learnjava/basic/chapC/operadores/OperadoresAritmeticos.java
@@ -1,5 +1,47 @@
 package br.com.learnjava.basic.chapC.operadores;
 
+/**
+ * 
+ * @author Cleiton Oliveira
+ * 
+ * O java possui 5 operadores aritiméticos:
+ *
+ */
+
+//	+  	-> 	soma
+//	- 	-> 	subtração
+//	/	-> 	divisão
+//	*	-> 	multiplicação
+//	%	->	resto de divisão
+
 public class OperadoresAritmeticos {
+	
+	void soma() {
+		int valor1 = 11;
+		int valor2 = 11;
+		long valor3 = 22;
+		
+		int resultado1;
+		int resultado2;
+		long resultado3;
+		
+		/** o operador de soma realiza a soma de tipos numéricos do java */
+		resultado1 = valor1 + valor2 + 11;
+		
+		/** é preciso se atentar que quando houver uma operação com mais de um tipo
+			caso necessário determinar o tipo do resultado da operação, devem ser realizados as conversões necessárias */
+		resultado2 = valor1 + (int) valor3;
+		
+		/** no exemplo abaixo não foi necessária uma conversão,
+		 	a varáivel 'resultado3' é do tipo long.
+		 	
+		 	Sempre que for realizada uma operação matemática, o retorno será do tipo de maiorssssssssssss capacidade,
+		 	ou seja, no exemplo abaixo, long é maior que int, logo, o resultado será do tipo long.
+		 	
+		 	Se for necessário que o resultado seja do tipo int, deve ser observado a operação acima, 
+		 	que alimenta a variável de 'resultado2' */
+		resultado3 = valor1 + valor3;
+	}
+	
 
 }
